765876c93d3e95e0f2aac15c491342f544cb5202
das-nayan <60134057+das-nayan@users.noreply.github.com>
2025-09-27 10:35:57 +0000

    Deploying to gh-pages from @ das-nayan/das-nayan.github.io@a8e55423d54c350472a29634bafc3ed8d97f16a8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "das-nayan/nayan.github.io",
+    "data-repo": "das-nayan/das-nayan.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
